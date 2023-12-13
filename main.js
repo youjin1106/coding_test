0c1342b8ef22a3dadecdbfcbcb1fa392a9cd9161
jyj <jjnote22@gmail.com>
2023-12-13 22:57:46 +0900

    BJ 11540

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 const fs = require('fs');
-const input = fs.readFileSync('testFile.txt').toString().split('\n');
+const input = fs.readFileSync('testFile.txt').toString().trim().split('\n');
 
-const count = input[0];
+const n = Number(input[0]);
+const arr = []
+for(let i = 1 ; i <= n ; i += 1){
+  const [x,y] =input[i].split(' ').map(Number);
+  arr.push([x,y]);
+}
+
+//정렬
+arr.sort((a,b) => {
+  //x좌표가 같지 않을 때 x좌표로만 비교
+  if(a[0]!=b[0]) return a[0]-b[0]
+  //x좌표가 같은 경우 y좌표를 비교
+  else return a[1]-b[1]
+})
 
 let result = '';
-for( let i = 0; i<4 ; i+=1){
-  for(let j = 0 ; j < i ; j+=1 ){
-    result += '*'
-  }
-  result += '\n';
-}
+arr.forEach(a => result += a[0] + ' ' + a[1] + '\n');
+
 console.log(result);
