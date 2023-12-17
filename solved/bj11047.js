49cfd56444d32200afd314f3bf29a0bdc3d01611
jyj <jjnote22@gmail.com>
2023-12-17 20:57:14 +0900

    bj11047

---
+++ solved/bj11047.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const input = fs.readFileSync('testFile.txt').toString().trim().split('\n');
+
+let [n, k] = (input[0]).split(' ').map(Number);
+const coinList = [];
+let result = 0;
+for(let i = 1 ; i <= n ; i += 1 ){
+  coinList.push(Number(input[i]));
+}
+
+for(let j = n - 1 ; j >= 0 ; j -= 1){
+  result += parseInt(k / coinList[j]);
+  k %= coinList[j];
+}
+
+console.log(result);
