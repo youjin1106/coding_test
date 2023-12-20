e52bb67e2fe2e9aee02f22fad5bf885e85c06f9a
jyj <jjnote22@gmail.com>
2023-12-21 00:08:24 +0900

    bj1946

--- solved/bj1789.js
+++ solved/bj1789.js
@@ -1,11 +1,25 @@
 const fs = require('fs');
-const input = fs.readFileSync('testFile.txt').toString().trim().split('\n');
-const s = Number(input[0]);
-let count = 0;
-let sum = 0;
-let n = 0;
-while(s>sum){
-  n+=1
-  sum+=n;
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const testCase = Number(input[0]);
+let line = 1;
+for(let i = 0; i < testCase ; i += 1){
+  const people = Number(input[line]);
+  const arr = [];
+  for(let j = line+1 ; j <= line+people ; j += 1){
+    const data = input[j].split(' ').map(Number);
+    arr.push(data);
+  }
+  //첫번째 순위 기준 오름차순 정렬
+  arr.sort((a, b) => a[0]- b[0]);
+
+  let count = 0;
+  let minVal = 100001;
+  for(const [a, b] of arr){
+    if(b < minVal){
+      minVal = b;
+      count += 1;
+    }
+  }
+  console.log(count);
+  line += people+1;
 }
-console.log(n-1);
